f4fd0677a9d0087960ca6043b0a6d66b807d4337
Joe Kutner <jpkutner@gmail.com>
2015-04-25 00:00:14 -0500

    removing ruby-maven

--- Rakefile
+++ Rakefile
@@ -2,5 +2,7 @@ require 'warbler'
 Warbler::Task.new
 
 task "assets:precompile" do
+  `rm -rf vendor/ruby-1.9.3-jruby-1.7.19/lib/ruby/gems/shared/bin/rmvn`
+  `rm -rf vendor/ruby-1.9.3-jruby-1.7.19/lib/target`
   Rake::Task["war"].execute
 end
--- config/warble.rb
+++ config/warble.rb
@@ -1,6 +1,10 @@
 Warbler::Config.new do |config|
   config.features = %w(executable)
   config.jar_name = "heroku-warbler-example"
-  config.bundle_without += ["package"]
-  config.gem_excludes = [/^(rmvn|ruby-maven)\//]
+  # config.bundler = false
+  # config.gems += ["rack"]
+  # config.bundle_without += ["package"]
+  # config.gems -= ["ruby-maven"]
+  # config.gems -= ["warbler"]
+  # config.gem_excludes = [/^(rmvn|ruby-maven)\//]
 end
