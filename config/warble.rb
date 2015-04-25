bf3f75a8bd0de96257970fd19089426eb6191753
Joe Kutner <jpkutner@gmail.com>
2015-04-25 11:48:34 -0500

    clean up config

--- config/warble.rb
+++ config/warble.rb
@@ -1,6 +1,5 @@
 Warbler::Config.new do |config|
   config.features = %w(executable)
   config.jar_name = "heroku-warbler-example"
-  # config.dirs = %w(config) # remove vendor
   config.excludes = FileList["vendor/**/*"]
 end
